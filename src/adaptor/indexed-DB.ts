78aa1ee51b66f926c93d37dd94167c46338abf9e
wsafight <984292420@qq.com>
2022-11-29 13:12:00 +0800

    #FEAT: change indexedDB

--- src/adaptor/indexed-DB.ts
+++ src/adaptor/indexed-DB.ts
@@ -1,17 +1,51 @@
 import { StorageAdaptor } from "../utils"
 
+interface IndexedDBAdaptorParams {
+  dbName: string
+  storeName: string
+  IDBOptions: IDBObjectStoreParameters
+  IDBIndexList: {
+    name: string
+    keyPath: string | string[]
+    options?: IDBIndexParameters
+  }[]
+}
+
 export class IndexedDBAdaptor implements StorageAdaptor {
 
-    constructor() {
-        
-    }
- 
-    getItem(key: string) {
-        return null
-    }
+  readonly db: IDBDatabase
+  readonly storeName: string
 
-    setItem(key: string, value: string) {
-        console.log(key)
-        console.log(value)
+  constructor({
+    dbName,
+    storeName,
+    IDBOptions = {},
+    IDBIndexList = []
+  }: IndexedDBAdaptorParams) {
+    this.storeName = storeName
+    const request = indexedDB.open(dbName)
+    const { result } = request
+
+    this.db = result
+
+    request.onupgradeneeded = () => {
+      if (!result.objectStoreNames.contains(storeName)) {
+        const store = result.createObjectStore(storeName, { ...IDBOptions })
+        IDBIndexList.forEach(index => {
+          store.createIndex(index.name, index.keyPath, index.options)
+        })
+      }
     }
+  }
+
+
+  getItem(key: string): string {
+    const value = this.db.transaction(this.storeName).objectStore(this.storeName).get(key)
+    return value.result?.data || ''
+  }
+
+  setItem(key: string, value: string) {
+    console.log(key)
+    console.log(value)
+  }
 }
